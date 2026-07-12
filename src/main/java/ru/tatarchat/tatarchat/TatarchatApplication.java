b80ca70ce9398e60c5befd2c066a6f66f28f782e
e.belousov <e.belousov@virtusystems.ru>
2026-07-12 12:32:57 +0300

    initial commit

---
+++ src/main/java/ru/tatarchat/tatarchat/TatarchatApplication.java
@@ -0,0 +1,13 @@
+package ru.tatarchat.tatarchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatarchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TatarchatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/tatarchat/tatarchat/TatarchatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tatarchat.tatarchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TatarchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
